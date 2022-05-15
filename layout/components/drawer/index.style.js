db135b9929b4fef0c47297bf98ad4c6df93ba213
K1 <kayvanbahman@gmail.com>
2022-05-16 00:02:33 +0430

    admin create page setup

--- layout/components/drawer/index.style.js
+++ layout/components/drawer/index.style.js
@@ -6,4 +6,29 @@ export const DrawerContainer = styled.div`
   width: 100%;
   height: 100%;
   background-color: #eee;
+  .container {
+    display: flex;
+    flex-direction: column;
+    width: 40%;
+    margin-left: auto;
+    margin-right: 0;
+  }
+  .label {
+    margin-bottom: 16px;
+  }
+  .input {
+    margin-bottom: 16px;
+    background-color: #ddd;
+    border-radius: 9px;
+    height: 30px;
+  }
+  .button {
+    background-color: orange;
+    border-radius: 8px;
+    padding: 4px 8px;
+    border: none;
+    width: 30%;
+    margin-right: auto;
+    margin-top: 3rem;
+  }
 `;
