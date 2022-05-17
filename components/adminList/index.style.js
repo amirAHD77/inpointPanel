7046b90689a20241d0e175a13f179e19552658bf
Amir <amirhossein7731377@gmail.com>
2022-05-17 23:32:12 +0430

    +

--- components/adminList/index.style.js
+++ components/adminList/index.style.js
@@ -25,6 +25,61 @@ const AdminListContainer = styled.div`
     margin-top: 32px;
   }
 
+  .table > :not(:first-child) {
+    border-top: 2px solid green;
+  }
+
+  th {
+    text-align: center;
+  }
+
+  td {
+    text-align: center;
+  }
+
+  .functionsContainer {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+  }
+
+  .functionsContainer > div {
+    margin: 0 8px;
+  }
+
+  .btn {
+    border: 1px solid;
+    padding: 2px 8px;
+  }
+
+  .disable {
+    border-color: purple;
+    color: purple;
+  }
+
+  .delete {
+    border-color: red;
+    color: red;
+  }
+
+  .edit {
+    color: orange;
+    border-color: orange;
+  }
+
+  .logs {
+    color: yellowgreen;
+    border-color: yellowgreen;
+  }
+
+  .name {
+    width: 150px;
+  }
+
+  .price {
+    width: 180px;
+  }
+
   @media screen and (max-width: 768px) {
   }
 `;
