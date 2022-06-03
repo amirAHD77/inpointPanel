15d45480d219815f5bad578014fef91c563e60c7
Amir <amirhossein7731377@gmail.com>
2022-06-03 20:06:52 +0430

    +

--- components/login/index.jsx
+++ components/login/index.jsx
@@ -73,7 +73,8 @@ const Login = (props) => {
             <Form className="form">
               <div className="header">
                 <img
-                  src="/images/inpoint connect logo PNG.png"
+                  src="/images/gharbOnline.png"
+                  width
                   alt="inPoint img"
                   width={"50%"}
                 />
@@ -114,10 +115,10 @@ const Login = (props) => {
           )}
         </Formik>
         <div
-          onClick={() => window.open("tel:09010187117")}
+          // onClick={() => window.open("tel:09010187117")}
           className="supportText"
         >
-          تماس با پشتیبانی : 09010187117
+          کلیه حقوق مادی و معنوی پلتفرم متعلق به آموزشگاه غرب آنلاین می باشد
         </div>
       </div>
     </LoginContainer>
